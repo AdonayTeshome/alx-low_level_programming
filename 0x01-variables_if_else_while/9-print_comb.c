cb8ce3c79710c0dc1648c30e7542c44d81d23ce4
AdonayTeshome <teshome12adonay@gmail.com>
2023-12-25 23:00:13 +0300

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ * Description: A program that prints all possible combinations of single-digit
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int i = 48;
+	int space = 32;
+	int comma = 44;
+
+	while (i <= 57)
+	{
+		if (i == 57)
+		{
+			space = 36;
+			comma = 28;
+		}
+		putchar(i);
+		putchar(comma);
+		putchar(space);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
